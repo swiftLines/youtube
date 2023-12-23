feb820227d6dc4d6edde4c301f76aa9d30471ffa
Jeremy Underwood <underwood.j248@gmail.com>
2023-12-23 17:18:43 -0500

    add access to streams and download capabilities

--- youtube.py
+++ youtube.py
@@ -5,5 +5,9 @@ from tkinter import filedialog
 def download_video(url, save_path):
   try:
     yt = YouTube(url)
+    streams = yt.streams.filter(progressive=True, file_extension='mp4')
+    highest_res_stream = streams.get_highest_resolution()
+    highest_res_stream.download(output_path=save_path)
+    print('Video downloaded successfully!')
   except Exception as e:
     print(e)
